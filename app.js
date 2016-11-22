dd9f23086a1161e60958cbdd883864f8f6343cc7
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2016-11-22 13:18:21 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const MongoClient = require('mongodb').MongoClient;
+
+const app = express();
+
+app.use( express.static('public') )
+
+const ENVIRONMENT = process.env.ENVIRONMENT || 'development';
+let urlDB = 'mongodb://localhost:27017/test';
+
+// Connection URL
+if (ENVIRONMENT === 'production') {
+	urlDB = 'mongodb://admin:admin12345@ds047030.mlab.com:47030/restaurants-demo';
+}
+
+console.log(`Connecting to DB: ${urlDB}`)
+
+MongoClient.connect(urlDB, (err, db) => {
+
+  if (err) throw("There were problems connecting to the DB server...");
+
+	app.get('/restaurants', (req, res) => {
+
+	  db.collection('restaurants')
+	      .find({})
+	      .limit(10) // cursor
+	      .toArray() // promise
+	      .then( restaurants => { res.json(restaurants) } );
+
+	});
+
+});
+
+app.listen(3000, ()=> console.log("Listening on 3000..."))
